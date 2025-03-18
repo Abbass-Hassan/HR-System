2f97d9a382e5ce821cdbdb706432310e0e631d3c
Abbass Hassan <abbasshassan@Abbasss-MacBook-Air.local>
2025-03-18 04:29:44 +0200

    Finish HR sidebar with collapsible menus

---
+++ frontend/src/views/HR/Dashboard/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Dashboard.css";
+
+function Dashboard() {
+  return (
+      <div>
+        <h1>Dashboard</h1>
+        <p>Welcome to the HR dashboard!</p>
+      </div>
+  );
+}
+
+export default Dashboard;
