106f105b353c84fda4cc4cdbaa5199e93b2bd8dd
Rawan Ghobar <rawanghobar@gmail.com>
2025-03-22 07:27:35 +0200

    Create AuthController and routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,21 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Common\AuthController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\User\UserController;
 
-Route::get('/',[AuthController::class,"getQuestions"]);
+Route::group(["prefix" => "v0.1"], function(){
+    //Authenticated Routes
+    Route::group(["middleware" => "auth:api"], function(){
+        //Admin Routes
+        Route::group(["prefix" => "admin", "middleware" => "isAdmin"], function(){
+            Route::get('/dashboard', [UserController::class, "getUsers"]);
+        });
+    });
+
+    //Unauthenticated routes
+    Route::group(["prefix" => "guest"], function(){
+        Route::post('/login', [AuthController::class, "login"]);
+        Route::post('/signup', [AuthController::class, "signup"]);
+    });
+});
