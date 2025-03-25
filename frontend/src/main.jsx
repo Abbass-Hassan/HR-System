5288f93ee91f05cf35ffa5da5255dfdaff56f853
Ali Al Ahmad <ali.alahmad.cs@gmail.com>
2025-03-25 19:23:18 +0200

    Added a customized alert

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
+import { Fragment } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { ToastProvider } from './context/Toast/Toast.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <Fragment>
+    <ToastProvider>
+      <App />
+    </ToastProvider>
+  </Fragment>
 )
