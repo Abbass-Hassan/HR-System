1717f44f473883300c4501de6a3daa7924c07f3f
Ali Al Ahmad <ali.alahmad.cs@gmail.com>
2025-03-26 03:29:10 +0200

    fixed context

--- frontend/src/context/User/UserContext.jsx
+++ frontend/src/context/User/UserContext.jsx
@@ -1,29 +1,21 @@
 import { createContext, useEffect, useState } from 'react'
 import { jwtDecode } from 'jwt-decode'
-import api from '../../services/Api'
 
 export const UserContext = createContext()
 
 export const UserProvider = ({ children }) => {
   const [token, setToken] = useState(localStorage.getItem('token'))
-  const [user, setUser] = useState(token ? jwtDecode(token) : null)
+  const [user, setUser] = useState(
+    localStorage.getItem('user')
+      ? JSON.parse(localStorage.getItem('user'))
+      : token
+      ? jwtDecode(token)
+      : null
+  )
 
   useEffect(() => {
-    if (token) {
-      fetchUser()
-    } else {
-      setUser(null)
-    }
-  }, [])
-
-  const fetchUser = async () => {
-    try {
-      const response = await api.get('api/v0.1/profile')
-      setUser(response.data.user)
-    } catch (error) {
-      console.log(error)
-    }
-  }
+    localStorage.setItem('user', JSON.stringify(user))
+  }, [user])
 
   return (
     <UserContext.Provider value={{ user, setUser, token, setToken }}>
